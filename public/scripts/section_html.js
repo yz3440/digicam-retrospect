e9871759c3843bde0c36769a7dac07eb8dd1d8be
yz3440 <yufeng-zhao@outlook.com>
2021-10-20 11:26:06 -0400

    download online images to local

--- public/scripts/section_html.js
+++ public/scripts/section_html.js
@@ -4,33 +4,32 @@ the
 <b>image sensor</b> is crucial for better image quality.
 
 </p>
-<img src="https://s23527.pcdn.co/wp-content/uploads/2018/05/canonapsh120.jpg.optimal.jpg"
+<img src="assets/sensor.jpg"
 width='200px'>
 <p>
 Over the past decades, digital cameras benefit from advanced electronic manufacturing. As
 semiconductors get smaller, more pixels can be packed into a single sensor, resulting in a
 sharper
 and clearer image.
-</p>`
+</p>`;
 
 let image_sensor_technology = ` <p>
 <b style='color:green'>CMOS <sub>(Complementary Metal–Oxide–Semiconductor)</sub></b>
 <p>The <span style='color:green'>CMOS</span> sensor technology is increasingly being used in today's cameras, allowing users to shoot 1080p video and apply complex imaging effects with ease.</p>
 <b style='color:skyblue'>CCD <sub>(Charge-Coupled Device)</sub></b>
 <p>Traditionally, <span style='color:skyblue'>CCD</span> sensors have been thought to produce better-looking images with less visual noise and distortion, but they draw more power and provide slower data-throughput speed.</p>
-<img src="https://meroli.web.cern.ch/img/lecture/spatial/clip_image002.jpg" width='400px'>
+<img src="assets/sensor-compare.jpg" width='400px'>
 <br>
 There are also cameras using <a href='https://www.sigma-global.com/en/cameras/dp-series/technology/' style='color:purple'>Foveon X3</a> , a proprietary technology from Sigma.
 
 
-</p>`
-
+</p>`;
 
 let sensor_form_factor = ` <p>
 A <b>Full-Frame</b> digital camera is one with an image sensor format that
 is the same size as <b>35 mm format film</b>.
 </p>
-<img src="https://i.ebayimg.com/images/g/6GUAAOSwMilciT0T/s-l1600.jpg" width='200px'>
+<img src="assets/film.jpg" width='200px'>
 <p>
 Historically, 35 mm was considered a small
 film format compared with medium format, large format and even larger. However, due to high
@@ -41,28 +40,27 @@ rate of larger digital image sensor, <b>smaller form factors</b> are much more c
 If under the same glass, smaller sensors cover a <b>smaller field of view</b> and thus a
 <b>magnified perspective</b>.
 </p>
-`
+`;
 
 let lens_system = `  <p>
 Without doubt, more sphisticated cameras with <span style='color:#1C415A'><b>Interchangeable Lens
         System</b></span> takes a relatively professional approach, therefore, equipped with larger
 sensors for quality lens families.
 </p>
-<img src="https://hnsfpau.imgix.net/5/images/detailed/128/ILCE6000LB.01Main.jpg?fit=fill&bg=0FFF&w=295&h=165&auto=format,compress" width='200px'>
+<img src="assets/digicam-1.jpg" width='200px'>
 <p>
 Manufacturers prefer small sensors for cameras with a <span style='color:#ED6168'><b>Built-In Lens
         System</b></span>. Besides cost saving in the low-end market, the optical system is also a
 consideration. Since smaller sensors crop out the edges and enlarge the center of the frame, the
 zoom range of a lens expands as well.
 </p>
-<img src="https://media.4rgos.it/i/Argos/3775332_R_Z001A?w=750&h=440&qlt=70" width='150px'>
+<img src="assets/digicam-2.jpg" width='150px'>
 
 <p>
 For example, a light-weight zoom lens (4-20mm) on a <b>1-inch</b> sensor could covers the same range
 of a
 heavy zoom (24-120mm) on a <b>Full-Frame</b> sensor.
-</p>`
-
+</p>`;
 
 let body_weight = `   <p>
 Generally, digital cameras get lighter as time goes. Althogh the most high-end camera of the day
@@ -70,16 +68,14 @@ are
 be quite heavy, we see a <span style='color:#1C415A'><b>Interchangeable Lens
         System</b></span> can be offerred with less and less weight.
 
-</p>`
-
+</p>`;
 
 let body_size = `<p>
 Similarly, we see the body dimensions have shrinked over time, a little more drastically than the
 weight decrease.
-</p>`
-
+</p>`;
 
 let sensor_size_per_body_size = `<p>
 
 
-`
+`;
